1f33e69c31498c33ccd541c77fb5dff43873220b
Dan Dickson <dandickson24@gmail.com>
2023-04-01 16:34:09 -0400

    new

---
+++ app.js
@@ -0,0 +1,29 @@
+-What is HTTP?
+Hyper Text Transfer Protocol
+
+-What is a URL?
+Uniform Resource Locatoran address for a internet resource
+
+-What is DNS?
+Domain Name System - like a switchboard for urls - converts urls to IP addresses
+
+-What is a query string?
+passes data value pairs into the url
+
+-List two HTTP Verbs and their use cases.
+GET - gets data from server
+POST - sends new data to the server
+
+-What is an HTTP request?
+its the request from the user to the server to execute the protocol 
+
+-What is an HTTP response?
+it is the answer to the request, so if the request was to go to a website it will send back the info for the site
+
+-What is an HTTP header? Give a couple examples of request and response headers you have seen.
+they give more info about the request or response
+request - host - accept - cookie
+response - last modified - content type
+
+-What happens when you type a URL in a browser?
+the url goes to a dns to be converted into an ip address - then a request is made to that address - then the server responds and the browser interprets the response
